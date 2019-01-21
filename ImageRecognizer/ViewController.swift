12899ed1bf4078a45587fbb8866201d168883497
Dan Korkelia <mail@dankorkelia.com>
2019-01-21 21:08:40 +0000

    Add Basic project

--- ImageRecognizer/ViewController.swift
+++ ImageRecognizer/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    @IBOutlet weak var labelView: UIView!
+    @IBOutlet weak var identifierLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        labelView.alpha = 0.5
+        labelView.layer.cornerRadius = 4.5
+        
+        let captureSession = AVCaptureSession()
+        /// Presets
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        
+        /// Get Access to Camera Frame Layer
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+//         print("Camera was able to capture a frame", Date())
+        
+        ///Analyse what camera is showing
+        
+        guard let cvPixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, Error) in
+            
+            print("There is an \(String(describing: Error))")
+            
+            guard let result = finishedRequest.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = result.first else { return }
+            
+            DispatchQueue.main.async {
+                self.identifierLabel.text = firstObservation.identifier
+                self.confidenceLabel.text = "\(firstObservation.confidence)"
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: cvPixelBuffer, options: [:]).perform([request])
     }
 
+
+    
 
 }
 
---
+++ README.md
@@ -0,0 +1,28 @@
+# Image classification using Apple's Vision Framework
+
+This sample app uses Resnet50 model for image classification to identify everyday objects. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+iOS 11+ compatible device requered to run the app
+
+### Installing
+
+Download or clone project.
+
+End with an example of getting some data out of the system or using it for a little demo.
+
+
+## Built With
+
+* [Restnet50](https://developer.apple.com/machine-learning/model-details/ResNet50.txt) - ML Model used.
+* [Vision Framework](https://developer.apple.com/documentation/vision)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
